d5b5daac955ed03f2fab0ecf673e89de5690cef0
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-02-17 16:18:47 +0200

    Task 7

--- pz_1_11/script.js
+++ pz_1_11/script.js
@@ -93,3 +93,26 @@ function magicArrayFunction(elementsCount) {
     console.log("Odd numbers is " + magicArrayOfOdd);
 }
 
+// Task 7
+function magic2DArrayFunction() {
+    let magicArray = [[],[],[],[],[]];
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            magicArray[i][j] = Math.round(-50 + Math.random() * (100));
+        }
+    }
+    console.log(magicArray);
+    setTimeout(()=>{
+        let newMagicArray = magicArray.slice();
+        for (let i = 0; i < 5; i++) {
+            if (newMagicArray[i][i] < 0) {
+                newMagicArray[i][i] = 0;
+            }
+            if (newMagicArray[i][i] > 0) {
+                newMagicArray[i][i] = 1;
+            }
+        }
+        console.log(newMagicArray);
+    }, 2000);
+}
+
