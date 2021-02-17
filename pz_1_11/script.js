4ed114b55919a06b5520f85571b39bc8dd65b63d
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-02-17 16:17:25 +0200

    Task 6

--- pz_1_11/script.js
+++ pz_1_11/script.js
@@ -59,3 +59,37 @@ function isD(n,x,y){
     return res;
 }
 console.log(isD(13,3,4));
+
+// Task 6
+function magicArrayFunction(elementsCount) {
+    function random(min, max) {
+        return Math.round(min + Math.random() * (max - min));
+    }
+    let magicArrayOfOdd = [];
+    let magicArray = [];
+    for (let i = 0; i < elementsCount; i++) {
+        magicArray.push(random(1,100));
+    }
+    console.log(magicArray);
+    let maxValue = 0;
+    let minValue = 1000;
+    let elementsSum = 0;
+    for (let i = 0; i < magicArray.length; i++) {
+        if (magicArray[i] > maxValue) {
+            maxValue = magicArray[i];
+        }
+        if (magicArray[i] < minValue) {
+            minValue = magicArray[i];
+        }
+        if (magicArray[i] % 2 == 1) {
+            magicArrayOfOdd.push(magicArray[i]);
+        }
+        elementsSum += magicArray[i];
+    }
+    console.log("Biggest number is " + maxValue);
+    console.log("Lowest number is " + minValue);
+    console.log("Sum of elements is " + elementsSum);
+    console.log("Average is " + elementsSum / magicArray.length);
+    console.log("Odd numbers is " + magicArrayOfOdd);
+}
+
