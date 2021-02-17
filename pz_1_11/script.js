fc189ecb6779839660165ac9fad2010fd6e2bc2a
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-02-17 16:16:59 +0200

    Task 5

--- pz_1_11/script.js
+++ pz_1_11/script.js
@@ -35,3 +35,27 @@ function calculate (num1, num2, num3) {
     return (num1 + num2 + num3) / 3;
 }
 
+//Task 5
+//Part 1
+function isDivisible(n,x,y){
+    let end;
+    if(n%x==0 && n%y==0){
+        end="Success";
+    }else{
+        end="Error";
+    }
+    return end;
+}
+console.log(isDivisible(12,5,4));
+//Part 2
+const isd= (n,x,y) =>{
+    const s=(n%x==0) && (n%y==0);
+    return s;
+};
+console.log(isd(12,3,4))
+//Part 3
+function isD(n,x,y){
+    let res=(n%x==0 && n%y==0) ? 'Success' : 'Error';
+    return res;
+}
+console.log(isD(13,3,4));
