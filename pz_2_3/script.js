8b5f8adfd25fbb547ecf76b5311dd1f6e0886bac
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-03-01 14:04:21 +0200

    PZ2.3  Task 2

--- pz_2_3/script.js
+++ pz_2_3/script.js
@@ -6,3 +6,16 @@ app.get("/", function(request, response){
     response.end("Hello from Express!");
 });
 app.listen(3000);
+
+//Task 2
+const colors = require("colors");
+const sound = require("sound-play");
+const play = require('audio-play');
+const load = require('audio-loader');
+
+load('beep.mp3').then(play);
+sound.play("beep.mp3", 1);
+sound.play("beep.mp3").then((response) => console.log("done"));
+console.log(colors.rainbow("Slava Ukraine!"))
+
+//T
