6a895e63da79c941cf992d280f5f79cd8f05ca23
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-03-01 12:04:46 +0200

    Task 2 pz1.13

--- pz_1_13/script.js
+++ pz_1_13/script.js
@@ -34,3 +34,30 @@ function dayOfWeek() {
         }
     }
 }
+
+dayOfWeek();
+
+//Task 2
+function chessBoard(length, width) {
+    this.length = length;
+     this.width = width;
+    let string = '';
+    let str = '';
+    for (;length > 0; length--){
+        string += '\n'
+        string += length;
+        for (let y = 1; y < width + 1; y++){
+            if ( y % 2 !== 0){
+                string += " #"
+            } else string += " @"
+        }
+    }
+    console.log(string)
+    for (let i = 0; i < width; i++) {
+        str += String.fromCodePoint(65 + i) + ' ';
+    }
+    console.log('  ' + str);
+}
+
+let chess = new chessBoard(9, 9);
+console.log(chess);
