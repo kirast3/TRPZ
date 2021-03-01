f2b77b565d90b9a27a791a5e1ebd6e6811350de8
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-03-01 15:41:47 +0200

    PZ2.3 task 4

--- pz_2_3/script.js
+++ pz_2_3/script.js
@@ -15,7 +15,6 @@ const colors = require("colors");
 const sound = require("sound-play");
 
 sound.play("D:\\TRPZ\\pz_2_3\\beep.mp3", 1);
-sound.play("D:\\TRPZ\\pz_2_3\\beep.mp3").then((response) => console.log("done"));
 console.log(colors.rainbow("Slava Ukraine!"))
 
 //Task 3
@@ -27,3 +26,25 @@ readline.question("enter path: ", textPath=>{
     console.log(`Absolute path: ${textPath} \n Filename: ${path.basename(textPath)}\n Extension: ${path.extname(textPath)}\n OS: ${os.type}` );
     readline.close();
 });
+
+
+//Task 4
+function coinTosser() {
+    const readline = require('readline');
+    let result = Math.round(Math.random());
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Head or Tail? (Enter 0 for Head; Enter 1 for Tail) ', (answer) => {
+        if (result === Number.parseInt(answer)) {
+            console.log("You're right");
+        }
+        else {
+            console.log("No, you're wrong");
+        }
+        rl.close();
+    });
+}
+
+coinTosser();
